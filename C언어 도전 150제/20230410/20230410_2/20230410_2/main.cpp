b04577543c7858a5a36acd4e664b4f34ae79f95f
JunHyeok Choi <65770938+Udtshi@users.noreply.github.com>
2023-04-10 20:06:43 +0900

    Add files via upload

---
+++ C언어 도전 150제/20230410/20230410_2/20230410_2/main.cpp
@@ -0,0 +1,16 @@
+/*
+2.  상수형 개념 배우기
+
+문제 : 정수형 상수 1과 실수형 상수 3.141592를 더하는 프로그램을 작성하시오.
+*/
+
+#include <stdio.h>
+
+#define a 1
+#define PI 3.141592
+
+int main(void) {
+	double sum;
+	sum = a + PI;
+	printf("%lf", sum);
+}
