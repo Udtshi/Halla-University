32514a6d1dadb2cc63eb0f74eb423d929d0559b8
JunHyeok Choi <65770938+Udtshi@users.noreply.github.com>
2023-05-16 21:50:34 +0900

    Add files via upload

---
+++ 만도 수업/20230516/main.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int A[3][3] = {0};
+	int B[3][3] = {0};
+	int C[3][3] = {0};
+	int a = 0;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("A행렬의 %d행 %d열을 입력: ", i + 1, j + 1);
+			scanf("%d", &A[i][j]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("B행렬의 %d행 %d열을 입력: ", i + 1, j + 1);
+			scanf("%d", &B[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	printf("상수 a를 입력: ");
+	scanf("%d", &a);
+	printf("\n");
+
+	printf("Mat (A) + Mat (B)\n");
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			C[i][j] = A[i][j] + B[i][j];
+			printf("%d\t", C[i][j]);
+
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	printf("Mat (A) - Mat (B)\n");
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			C[i][j] = A[i][j] - B[i][j];
+			printf("%d\t", C[i][j]);
+
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	printf("Mat (C) = a Mat (A)\n");
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			C[i][j] = a * A[i][j] ;
+			printf("%d\t", C[i][j]);
+
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
