c033d818ab1390a437cdcbab96ddd14130b9468d
JunHyeok Choi <65770938+Udtshi@users.noreply.github.com>
2023-05-28 00:57:39 +0900

    Add files via upload

--- 모빌리티프로그래밍 기초/20230524/Programming/20230524_5.cpp
+++ 모빌리티프로그래밍 기초/20230524/Programming/20230524_5.cpp
@@ -15,40 +15,46 @@ int rolling_dice(int n);
 int main (void){
     srand((unsigned)time(NULL));
 
-    int player_dice = 0;
-    int com_dice = 0;
+    int player_dice[3] = {0, 0, 0};
+    int com_dice[3] = {0, 0, 0};
+    int player_dice_sum = 0;
+    int com_dice_sum = 0;
 
 // 사용자 주사위 굴리는 부분
     printf("사용자 주사위=(");
     for(int i = 0; i < 3; i++){
         if(i < 2){
-            printf("%d, ", (1 + rand() % 6));
+            player_dice[i] += (1 + rand() % 6);
+            printf("%d, ", player_dice[i]);
         }
         else{
-            printf("%d", (1 + rand() % 6));
+            player_dice[i] += (1 + rand() % 6);
+            printf("%d", player_dice[i]);
         }
-        player_dice += (1 + rand() % 6);
+        player_dice_sum += player_dice[i];
     }
-    printf(")=%d\n", player_dice);
+    printf(")=%d\n", player_dice_sum);
 
 // 컴퓨터 주사위 굴리는 부분
     printf("컴퓨터 주사위=(");
     for(int i = 0; i < 3; i++){
         if (i <2){
-            printf("%d, ", (1 + rand() % 6));
+            com_dice[i] += (1 + rand() % 6);
+            printf("%d, ", com_dice[i]);
         }
         else{
-            printf("%d", (1 + rand() % 6));
+            com_dice[i] += (1 + rand() % 6);
+            printf("%d", com_dice[i]);
         }
-        com_dice += (1 + rand() % 6);
+        com_dice_sum += com_dice[i];
     }
-    printf(")=%d\n", com_dice);
+    printf(")=%d\n", com_dice_sum);
 
 // 누구 주사위가 더 큰지 비교하는 부분
-    if (player_dice > com_dice){
+    if (player_dice_sum > com_dice_sum){
         printf("사용자 승리\n");
     }
-    else if (player_dice < com_dice){
+    else if (player_dice_sum < com_dice_sum){
         printf("컴퓨터 승리\n");
     }
     else{
--- 모빌리티프로그래밍 기초/20230524/Programming/20230524_6.cpp
+++ 모빌리티프로그래밍 기초/20230524/Programming/20230524_6.cpp
@@ -5,47 +5,83 @@
 이름 : 최준혁
 날짜 : 2023/05/24
 */
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int balance(int money);
 int deposit(int money);
 int withdraw(int money);
-int menu();
+int menu(int n);
 
-int main(void){
-    int n;
-    menu();
-    switch(menu(n)){
+int main(void) {
+    // reflect the money in the account
+    int money = 0;
+    // reflect the menu
+    int n = 0;
+
+    while (n != 4) {
+        n = menu(n);
+        switch (n) {
         case 1:
-        balance();
-        break;
 
+            money = balance(money);
+            break;
         case 2:
-        deposit():
-        break;
+            money = deposit(money);
 
+            break;
         case 3:
-        withdraw();
-        break;
+            money = withdraw(money);
 
+            break;
         case 4:
-        break;
-
-        default:
+            printf("프로그램을 종료합니다.\n");
+            break;
+        }
     }
+    return 0;
 }
 
-int menu(int n){
+int menu(int n) {
     printf("********** Welcome to Express ATM **********\n");
     printf("<1> 잔액\n");
     printf("<2> 입금\n");
     printf("<3> 출금\n");
     printf("<4> 종료\n");
+    printf("********************************************\n");
     printf("메뉴를 입력하시오: \n");
     int menu = 0;
-    scanf("%d", &menu);
-    return n = menu;
+    scanf("%d", &n);
+    printf("\n");
+    return n;
 }
-int balance(int money){
-    printf("잔고는 %d원입니다.\n", money);
+int balance(int money) {
+    printf("잔고는 %d원입니다.\n\n", money);
+
+    return money;
 }
+int deposit(int money) {
+    printf("입금할 금액을 입력하시오: ");
+    int deposit = 0;
+    scanf("%d", &deposit);
+    money += deposit;
+    printf("입금 후 잔고는 %d원입니다.\n\n", money);
+
+    return money;
+}
+int withdraw(int money) {
+    printf("출금할 금액을 입력하시오: ");
+    int withdraw = 0;
+    scanf("%d", &withdraw);
+    printf("\n");
+    if (withdraw <= money) {
+        money -= withdraw;
+    }
+    else {
+        printf("잔액이 부족합니다.\n");
+    }
+    printf("출금 후 잔고는 %d원입니다.\n\n", money);
+
+    return money;
+}
+
+
